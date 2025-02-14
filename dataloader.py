ae1b040c1bfe48de16bdd27fbb58f22445638944
20220698 <a.raudvee@student.tue.nl>
2025-02-14 20:24:31 +0100

    update of dataloader + preprocessing given in class

--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,141 @@
+import os
+import time
+import matplotlib
+import sklearn
 import subprocess
 
+# visualizatoin 
+import matplotlib.pyplot as plt 
+
+# data wrangling
+import pandas as pd
+import numpy as np 
+
+# data preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.impute import SimpleImputer
+
+# the data intro has been tested with the following versions
+print("pandas        Tested version: 2.0.3   Your version: %s" % pd.__version__)
+print("numpy         Tested version: 1.21.5  Your version: %s" % np.__version__)
+print("matplotlib    Tested version: 3.5.3   Your version: %s" % matplotlib.__version__)
+print("scikit-learn  Tested version: 1.2.2   Your version: %s" % sklearn.__version__)
+
+# custom imports
 from config import USER_NAME
 
-# Define the command
-command = [
-    "wget", "-r", "-N", "-c", "-np",
-    "--user", f"{USER_NAME}", "--ask-password",
-    "https://physionet.org/files/widsdatathon2020/1.0.0/"
-]
+if not os.path.exists("./physionet.org"):
+    # Define the command
+    command = [
+        "wget", "-r", "-N", "-c", "-np",
+        "--user", f"{USER_NAME}", "--ask-password",
+        "https://physionet.org/files/widsdatathon2020/1.0.0/"
+    ]
 
-# Execute the command
-subprocess.run(command)
+        
+    # Execute the command
+    subprocess.run(command)
+
+# Check
+if os.path.exists("./physionet.org"):
+    print("DATA DOWNLOADED: CODE 1 - SUCCESS\n")
+else:
+    print("DATA WASN'T DOWNLOADED: CODE 0 - FAIL\n")
+
+
+# description
+description = pd.read_csv('./physionet.org/files/widsdatathon2020/1.0.0/data/WiDS_Datathon_2020_Dictionary.csv')
+description_dict = description.set_index('Variable Name').to_dict(orient='index')
+# data
+df = pd.read_csv('./physionet.org/files/widsdatathon2020/1.0.0/data/training_v2.csv')
+
+print("HEAD OF THE DATASET:")
+print(df.head())
+print("\n")
+
+# global vars for preprocessing
+test_size = 0.2 # proportion for train versus test+val split
+val_size = 0.5 # proportion for test versus val split
+random_state = 42  # random state is used to set a seed for randomness, which is only relevant for reproducibility purposes
+max_missing = 0.8  # maximum percentage of missing values for a column to be dropped
+
+start_time = time.time()
+print("PREPROCESSING EXECUTION:")
+
+# save features
+X = df.copy().drop(['hospital_death', 'patient_id', 'encounter_id', 'hospital_id', 'icu_id', # drop identifiers
+                    'apache_4a_hospital_death_prob', 'apache_4a_icu_death_prob', # drop APACHE scores
+                    'apache_2_bodysystem'], # drop because of similarity with apache_3j_bodysystem
+                   axis=1)
+# save target variable
+y = df['hospital_death'].copy()
+# save APACHE scores for later evaluation on train / test / validation data
+y_apache = df['apache_4a_hospital_death_prob'].copy()
+
+""" SPLIT DATA SET """
+# split the dataset into train and test+validation set
+(
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    y_apache_train,
+    y_apache_test,
+    ) = train_test_split(X, y, y_apache, 
+                         test_size=test_size, # used for testing and validation
+                         random_state=random_state # for reproducibility
+                        ) 
+# split the test set into test + validation set
+(
+    X_val,
+    X_test,
+    y_val,
+    y_test,
+    y_apache_val,
+    y_apache_test,
+    ) = train_test_split(X_test, y_test, y_apache_test, 
+                         test_size=val_size, # used for testing and validation
+                         random_state=random_state # for reproducibility
+                        ) 
+
+"""MISSING VALUES"""
+# drop columns with many missing values
+missing = X_train.isna().sum() > max_missing * len(X_train)
+missing = missing[missing].index
+X_train = X_train.drop(missing, axis=1)
+X_val = X_val.drop(missing, axis=1)
+X_test = X_test.drop(missing, axis=1)
+
+"""FURTHER PROCESSING PIPELINE"""
+# define pre-processing steps for numerical features
+num_transformer = Pipeline(steps=[("constant", VarianceThreshold()), # remove constant features
+                                  ("imputer", SimpleImputer(strategy="mean")),
+                                 ])
+# define preprocessing steps for categorical features
+cat_transformer = Pipeline(steps=[("encoder", OneHotEncoder(drop='first', sparse_output=False, handle_unknown="ignore"))])
+# create preprocessing pipeline
+prep_pipeline = ColumnTransformer(
+    transformers=[
+        ('num', num_transformer, make_column_selector(dtype_exclude=object)), # apply to columns NOT of type object (int or float)
+        ('cat', cat_transformer, make_column_selector(dtype_include=object)) # apply to columns of type object
+    ])
+# pipeline
+prep_pipeline.fit(X_train, y_train)
+# display(prep_pipeline) # disply preprocessing pipeline
+
+# transform data sets
+X_train = pd.DataFrame(prep_pipeline.transform(X_train), columns=prep_pipeline.get_feature_names_out())
+X_val = pd.DataFrame(prep_pipeline.transform(X_val), columns=prep_pipeline.get_feature_names_out())
+X_test = pd.DataFrame(prep_pipeline.transform(X_test), columns=prep_pipeline.get_feature_names_out())
+        
+"""PRINT STATS"""
+print("DONE\n")
+print("STATUS OF THE PREPROCESSING:")
+print("Time: %.2fs" % (time.time() - start_time))
+print("Train set: %s rows, %s columns" % X_train.shape)
+print("Validation set: %s rows, %s columns" % X_val.shape)
+print("Test set: %s rows, %s columns" % X_test.shape)
