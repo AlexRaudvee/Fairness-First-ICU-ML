9a67648fca986642c0b8b681aab551249fef43d5
20220698 <a.raudvee@student.tue.nl>
2025-02-28 20:32:13 +0100

    log reg modification

--- training/log_reg.py
+++ training/log_reg.py
@@ -32,7 +32,7 @@ X_train, X_test, y_train, y_test = train_test_split(X_final, y, test_size=0.2, r
 
 # Initialize Logistic Regression Model
 # Increase max_iter if the solver needs more iterations to converge
-model = LogisticRegression(random_state=42, max_iter=1000)
+model = LogisticRegression(random_state=42, class_weight="balanced", max_iter=1000)
 model.fit(X_train, y_train)
 
 # Predict on test set
